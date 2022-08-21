b9f2e86d008b2c280b265401b462d0a4347f7a88
yueChin <transuni@foxmail.com>
2022-08-21 19:24:55 +0800

    BP目录重整

--- BP/Assets/BP/GameLoop/GameLoop.cs
+++ BP/Assets/BP/GameLoop/GameLoop.cs
@@ -6,29 +6,33 @@ namespace Pipeline
     public partial class GameLoop
     {
         private EcsWorld m_GpWorld;
-        private EcsSystems m_GpSystem;
+        private EcsWorldSystems m_GpSystem;
 
         private EcsWorld m_PhysicWorld;
-        private EcsSystems m_PhysicSystem;
+        private EcsWorldSystems m_PhysicSystem;
         public void Init()
         {
             m_GpWorld = new EcsWorld();
-            m_GpSystem = new EcsSystems(m_GpWorld);
+            m_GpSystem = new EcsWorldSystems(m_GpWorld);
             m_GpSystem.InjectSystemToBaseSystem();
 
             m_PhysicWorld = new EcsWorld();
-            m_PhysicSystem = new EcsSystems(m_PhysicWorld);
+            m_PhysicSystem = new EcsWorldSystems(m_PhysicWorld);
             m_PhysicSystem.InjectSystemToBaseSystem(true);
         }
 
         public void Update()
         {
-            
+            m_GpSystem.Start();
+            m_GpSystem.Run();
+            m_GpSystem.Destroy();
         }
 
         public void FixedUpdate()
         {
-            
+            m_PhysicSystem.Start();
+            m_PhysicSystem.Run();
+            m_PhysicSystem.Destroy();
         }
     }
 }
